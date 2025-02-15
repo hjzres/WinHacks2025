4c2eb8b838bd8a313d2e96edc706c15f1a25b45f
W1ckle <oprutac@gmail.com>
2025-02-15 16:39:41 -0500

    DB Interface enhanced
    I'm going to kill someone

--- winhacks2025/database/Recipe.py
+++ winhacks2025/database/Recipe.py
@@ -21,3 +21,38 @@ class Recipe:
         if recipe_query is None:
             return None
         return cls(*recipe_query)
+    
+
+    @classmethod
+    def write(self, cur):
+        inserted_tuple = (
+            self.name,
+            self.official,
+            self.difficulty,
+            self.time,
+            self.xp_amount,
+            self.recipe,
+        )
+        
+        cur.execute(
+            "INSERT INTO Recipes(Name, XP) "
+            "VALUES (?, ?, ?, ?, ?, ?)",
+            inserted_tuple,
+        )
+    
+
+    @classmethod
+    def search(cls, cur, name_snip: str):
+        
+        recipe_query = cur.execute(
+            "SELECT * FROM Recipes WHERE Name LIKE ?", (name_snip + "%",)
+        ).fetchall()
+        recipe_list = []
+        for i in range(len(recipe_query)):
+            recipe_list.append(cls(*recipe_query[i]))
+        return recipe_list
+    
+    
+    @staticmethod
+    def count(cur):
+        return cur.execute("SELECT COUNT(*) FROM Recipe").fetchone()[0]
--- winhacks2025/database/test.py
+++ winhacks2025/database/test.py
@@ -6,7 +6,5 @@ con = sqlite3.connect("./recipes.db")
 cur = con.cursor()
 
 
-
 user1 = User.from_id(cur, 1)
-
 print(user1.id, user1.name, user1.xp, user1.level)
