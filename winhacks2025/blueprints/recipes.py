678d57c09615f6ef7ea84c8785e8c8d0e93c11e7
bellflwr <70229194+bellflwr@users.noreply.github.com>
2025-02-15 20:47:30 -0500

    yummers

--- winhacks2025/blueprints/recipes.py
+++ winhacks2025/blueprints/recipes.py
@@ -1,6 +1,14 @@
-from flask import Blueprint, render_template, send_from_directory
+from string import ascii_lowercase, digits
+
+from flask import Blueprint, abort, render_template, send_from_directory
 
 recipes = Blueprint("recipes", __name__)
+
+allowed_characters = set(ascii_lowercase + digits + "_")
+
+
+def validate_recipe_name(name: str) -> bool:
+    return all(char in allowed_characters for char in name)
 
 
 @recipes.route("/<string:recipe_name>/play")
@@ -8,7 +16,9 @@ def recipe_player(recipe_name: str):
     return render_template("recipe_player.html", recipe_name=recipe_name)
 
 
-@recipes.route("/<string:recipe_name>.json")
-def get_recipe(recipe_name: str):
-    print(recipe_name)
-    return send_from_directory("../recipes/", f"{recipe_name}.json")
+@recipes.route("/<string:recipe_name>/assets/<path:file>")
+def get_recipe(recipe_name: str, file: str):
+    print(recipe_name, file)
+    if not validate_recipe_name(recipe_name):
+        abort(400)
+    return send_from_directory(f"../recipes/{recipe_name}", file)
--- winhacks2025/static/scripts/recipe_player.js
+++ winhacks2025/static/scripts/recipe_player.js
@@ -1,5 +1,75 @@
-const recipe = await (
-    await fetch(`/recipes/${recipeName}.json`)
-).json();
+// let recipe;
 
-console.log(recipe);
+// async function loadRecipe() {
+//     const recipeURL = `/recipes/${recipeName}/assets/data.json`;
+//     const response = await fetch(recipeURL);
+//     recipe = await response.json();
+// }
+
+// // let currentStage = "collection";
+// loadRecipe();
+
+const recipeURL = `/recipes/${recipeName}/assets/data.json`;
+
+// fetch(recipeURL).then(function (response) {
+//     response.json().then(function (data) {
+//         recipe = data;
+//         console.log(recipe);
+//     })
+// })
+
+function initialize(data) {
+    fetch(recipeURL).then(function (response) {
+        response.json().then(function (recv) {
+            data.recipe = recv;
+            console.log(recv);
+
+            let items = {}
+            data.recipe.ingredients.forEach(element => {
+                items[element] = false;
+            });
+
+            data.currentCards.push(
+                {
+                    id: data.cardCounter,
+                    type: "collect",
+                    items: items
+                }
+            )
+            data.cardCounter++;
+        })
+    })
+
+
+}
+
+function addCard(data) {
+    let step = data.recipe.steps[data.currentStep];
+    data.currentCards.push(
+        {
+            id: data.cardCounter,
+            type: 'step',
+            text: step.instruction
+        }
+    );
+    data.currentStep++;
+    data.cardCounter++;
+}
+
+function collectableClick(data) {
+    data.card.items[data.item] = !data.card.items[data.item];
+    if (Object.values(data.card.items).every((a) => a)) {
+        data.offscreen = true;
+        setTimeout(() => nextCard(data), 300);
+    }
+}
+
+function nextCard(data) {
+    data.currentCards.splice(data.index, 1);
+    addCard(data);
+}
+
+function stepSubmit(data) {
+    data.offscreen = true;
+    setTimeout(() => nextCard(data), 300);
+}
