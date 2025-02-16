5b37c2dda23906964818b4fbcfdb94b5f25680ba
W1ckle <oprutac@gmail.com>
2025-02-16 11:19:29 -0500

    Bitchass case-insensitive git

--- winhacks2025/database/__init__.py
+++ winhacks2025/database/__init__.py
@@ -1,3 +1,3 @@
-from .recipe import Recipe as Recipe
-from .user import User as User
+from .Recipe import Recipe as Recipe
+from .User import User as User
 from .cursor import get_cursor as get_cursor
