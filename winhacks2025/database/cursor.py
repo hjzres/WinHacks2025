78bd0b7ea8afa49d88162a3139aadc94d75bef58
W1ckle <oprutac@gmail.com>
2025-02-16 11:03:53 -0500

    Reformating

--- winhacks2025/database/cursor.py
+++ winhacks2025/database/cursor.py
@@ -4,11 +4,13 @@ from flask import g
 
 DATABASE = "recipes.db"
 
+
 def get_db():
     db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
 
 def get_cursor():
     cur = getattr(g, "_cursor", None)
