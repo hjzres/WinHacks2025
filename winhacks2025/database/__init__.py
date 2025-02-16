0d60826dd85104519246c256542ed7181287f86c
W1ckle <oprutac@gmail.com>
2025-02-16 09:28:49 -0500

    Renamed interface file names for sanity purposes

--- winhacks2025/database/__init__.py
+++ winhacks2025/database/__init__.py
@@ -1,3 +1,3 @@
-from .Recipe import Recipe as Recipe
-from .User import User as User
+from .recipe import Recipe as Recipe
+from .user import User as User
 from .cursor import get_cursor as get_cursor
