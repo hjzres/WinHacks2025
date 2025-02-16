59c8713031fbab36a9b2faefb2413bec0bc89ad9
Andrei <142443853+hjzres@users.noreply.github.com>
2025-02-16 11:11:39 -0500

    You can see player and xp at the top right

--- winhacks2025/blueprints/home.py
+++ winhacks2025/blueprints/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from winhacks2025.database import Recipe, get_cursor
+from winhacks2025.database import Recipe, User, get_cursor
 
 home = Blueprint("home", __name__)
 
@@ -9,8 +9,22 @@ home = Blueprint("home", __name__)
 def home_page():
 	cur = get_cursor()
 	recipes = Recipe.return_all(cur)
+	user = User.from_id(cur, 1)
+	name = user.name
+	level = user.level
+	xp_remaining = user.xp - level * 250
+	if level < 2:
+		rank = "I"
+	elif level < 4:
+		rank = "II"
+	elif level < 8:
+		rank = "III"
+	elif level < 16:
+		rank = "IV"
+	else:
+		rank = "VI"
 	
 	print(recipes)
 	recipe_length = print(len(recipes))
 	
-	return render_template("home.html", recipes=recipes, recipe_length=recipe_length)
+	return render_template("home.html", recipes=recipes, recipe_length=recipe_length, name=name, level=level, xp_remaining=xp_remaining, rank=rank)
