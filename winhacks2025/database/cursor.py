03e8b4550272f5df99d9a439b3e7e2744fd95070
W1ckle <oprutac@gmail.com>
2025-02-15 20:27:37 -0500

    Added cursor for Andei

---
+++ winhacks2025/database/cursor.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+from flask import g
+
+DATABASE = "recipes.db"
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def get_cursor():
+    cur = getattr(g, "_cursor", None)
+    if cur is None:
+        cur = g._cursor = get_db().cursor()
+    return cur
